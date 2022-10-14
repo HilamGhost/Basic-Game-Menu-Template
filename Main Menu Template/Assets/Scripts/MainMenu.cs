aa081ab5a4e4caf050d1bdb0f53a064a172a88e9
HilamGhost <mert.erdogan789@gmail.com>
2022-10-14 12:17:59 +0300

    main

---
+++ Main Menu Template/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+namespace MenuTemplate
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MainMenu : MonoBehaviour
+    {
+        [Header("Buttons")]
+        [SerializeField] Button startButton;
+        [SerializeField] Button exitButton;
+
+        [Header("Button Sound")]
+        [SerializeField] AudioClip buttonSound;
+        AudioSource menuAudio;
+
+        UnityAction _menuFunctionList;
+
+        [Header("Custom Interactions ")]
+        [SerializeField] string firstScene;
+        private void Awake()
+        {
+            menuAudio = GetComponent<AudioSource>();
+        }
+        void Start()
+        {
+            SetMenuListeners();
+        }
+       
+        void SetMenuListeners() 
+        {
+            AssignButton(startButton,StartGame);
+            AssignButton(exitButton,ExitGame);
+        }
+        
+        void AssignButton(Button _button,UnityAction _call) 
+        {
+            _button.onClick.AddListener(_call);
+            _button.onClick.AddListener(PlayButtonSound);
+        }
+
+        void PlayButtonSound() 
+        {
+            menuAudio.PlayOneShot(buttonSound);
+        }
+       
+        /// <summary>
+        /// If you created an event, after finishing the assignment you have to clear the menu function
+        /// </summary>
+        void ClearMenuFunctionList() 
+        {
+            _menuFunctionList = null;
+        }
+
+        #region Custom Button Interactions
+        void StartGame()
+        {
+            //SceneManager.LoadScene(firstScene);
+            Debug.Log("Starting The Game");
+        }
+        void ExitGame()
+        {
+            //Application.Quit();
+            Debug.Log("Exiting The Game");
+        }
+        #endregion
+
+
+    }
+}
